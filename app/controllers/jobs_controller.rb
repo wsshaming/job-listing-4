a41a440184e5d55d7ea064b4ab113c1fb02cf26a
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-04-20 17:16:19 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,8 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  
+
   def index
-     @jobs = Job.all
+     @jobs = Job.where(:is_hidden => false)
    end
 
   def show
@@ -47,6 +47,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
